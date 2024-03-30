c62c7ab4c84d7331be901616cf96ec30ae92e209
Oskar Stasiak <stasiakoskar02@gmail.com>
2024-03-30 23:12:31 +0100

    Dodaj pliki projektów.

---
+++ AvHTTP/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvHTTP
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
