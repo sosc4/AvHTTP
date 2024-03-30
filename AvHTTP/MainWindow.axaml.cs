54ec1d6d3e3a91adfee5e2c744c4d3cf87441ff2
Oskar Stasiak <stasiakoskar02@gmail.com>
2024-03-31 00:04:14 +0100

    💄 Method buttons are now reactive to selection

--- AvHTTP/MainWindow.axaml.cs
+++ AvHTTP/MainWindow.axaml.cs
@@ -2,14 +2,20 @@ using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace AvHTTP
 {
     public partial class MainWindow : Window
     {
+
+        private string _currentMethod = "GET";
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Opened += MainWindow_Opened;
+            HighlightMethodButton("GET");
         }
 
         private void MainWindow_Opened(object? sender, EventArgs e)
@@ -24,12 +30,37 @@ namespace AvHTTP
 
         private void OnMethodSet(object? sender, RoutedEventArgs e)
         {
-
+            if (sender is Button button)
+            {
+                _currentMethod = button.Content?.ToString() ?? "GET";
+                HighlightMethodButton(_currentMethod);
+            }
         }
 
         private async void OnSendClick(object? sender, RoutedEventArgs e)
         {
 
         }
+
+        private void HighlightMethodButton(string method)
+        {
+            var methodName = method.ToLowerInvariant();
+            if (methodName.Length > 0)
+            {
+                methodName = char.ToUpperInvariant(methodName[0]) + methodName.Substring(1);
+            }
+
+            this.FindControl<Button>("GetButton").Background = Brushes.Gray;
+            this.FindControl<Button>("PostButton").Background = Brushes.Gray;
+            this.FindControl<Button>("PutButton").Background = Brushes.Gray;
+            this.FindControl<Button>("DeleteButton").Background = Brushes.Gray;
+
+            var button = this.FindControl<Button>($"{methodName}Button");
+            if (button != null)
+            {
+                button.Background = Brushes.HotPink;
+            }
+        }
+
     }
 }
